82380618a2907d26dcefd642e30fb1d01229971b
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-05-04 16:47:51 +0530

    Added : Ability to create Binary Tree

--- test/java/com/binarysearchtree/BinarySearchTree.java
+++ test/java/com/binarysearchtree/BinarySearchTree.java
@@ -16,4 +16,26 @@ public class BinarySearchTree {
         binaryTree.add(70);
         Assert.assertEquals(3, binaryTree.size());
     }
+
+    /**
+     * uc2 testcase to create binary tree
+     */
+    @Test
+    public void Elements_Added_To_Create_BinaryTree_Returns_Size13() {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        binaryTree.add(22);
+        binaryTree.add(40);
+        binaryTree.add(60);
+        binaryTree.add(95);
+        binaryTree.add(11);
+        binaryTree.add(65);
+        binaryTree.add(3);
+        binaryTree.add(16);
+        binaryTree.add(63);
+        binaryTree.add(67);
+        Assert.assertEquals(13, binaryTree.size());
+    }
 }
